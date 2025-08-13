1cc0f44c8d19b5741fc37850b1712f39d6268ef0
Michael McKIE <michael_mckie@hotmail.com>
2025-08-13 12:34:51 +0800

    Fixed icon issues when expanding/contracting the navigation panel

--- js/script.js
+++ js/script.js
@@ -1,30 +1,9 @@
-const toggleButton = document.getElementById('toggle-btn')
-const sidebar = document.getElementById('sidebar')
+function toggleSidebar() {
+  const sidebar = document.getElementById('sidebar');
+  const iconExpand = document.getElementById('icon-expand');
+  const iconCollapse = document.getElementById('icon-collapse');
 
-function toggleSidebar(){
-  sidebar.classList.toggle('close')
-  toggleButton.classList.toggle('rotate')
-  closeAllSubMenus()
-}
-
-function toggleSubMenu(button){
-
-  if(!button.nextElementSibling.classList.contains('show')){
-    closeAllSubMenus()
-  }
-
-  button.nextElementSibling.classList.toggle('show')
-  button.classList.toggle('rotate')
-
-  if(sidebar.classList.contains('close')){
-    sidebar.classList.toggle('close')
-    toggleButton.classList.toggle('rotate')
-  }
-}
-
-function closeAllSubMenus(){
-  Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
-    ul.classList.remove('show')
-    ul.previousElementSibling.classList.remove('rotate')
-  })
+  sidebar.classList.toggle('close');
+  iconExpand.classList.toggle('hidden');
+  iconCollapse.classList.toggle('hidden');
 }
