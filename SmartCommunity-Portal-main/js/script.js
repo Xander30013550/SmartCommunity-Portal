ca57477c70e65439f111ad98ac4a05a1c25958cb
Samuel Kirk <Samuel.kirk@outlook.com.au>
2025-08-26 10:32:51 +0800

    Delete SmartCommunity-Portal-main directory

--- SmartCommunity-Portal-main/js/script.js
+++
@@ -1,9 +0,0 @@
-function toggleSidebar() {
-  const sidebar = document.getElementById('sidebar');
-  const iconExpand = document.getElementById('icon-expand');
-  const iconCollapse = document.getElementById('icon-collapse');
-
-  sidebar.classList.toggle('close');
-  iconExpand.classList.toggle('hidden');
-  iconCollapse.classList.toggle('hidden');
-}
