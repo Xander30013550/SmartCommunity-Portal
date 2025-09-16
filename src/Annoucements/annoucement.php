68bb5be8bc6f06011b84d47eeddce482a08f1b6f
Noxferus <Samuel.kirk@outlook.com.au>
2025-09-16 10:01:36 +0800

    XANDER DON'T TOUCH THE FILE STRUCTURE IN DEV BRANCH

---
+++ src/Annoucements/annoucement.php
@@ -0,0 +1,18 @@
+<?php 
+use DateTime;
+final class Announcement
+{
+    public string $id       = '';
+    public string $priority = 'normal'; // high|normal|low
+    public string $title    = '';
+    public string $body     = '';
+    public ?DateTime $start = null;
+    public ?DateTime $end   = null;
+    public string $category = 'General';
+    public ?array $link     = null;     // ['url' => '', 'text' => '']
+
+    public function __construct(array $data) {
+        foreach ($data as $k => $v) $this->$k = $v;
+    }
+}
+?>
